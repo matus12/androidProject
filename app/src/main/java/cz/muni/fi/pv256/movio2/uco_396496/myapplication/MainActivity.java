17dd43eea9dd89cbd2d94d7ae3303646988ae212
Matúš Bačkor <matus.backor@gmail.com>
2017-11-03 02:19:52 +0100

    Implement task4

--- app/src/main/java/cz/muni/fi/pv256/movio2/uco_396496/myapplication/MainActivity.java
+++ app/src/main/java/cz/muni/fi/pv256/movio2/uco_396496/myapplication/MainActivity.java
@@ -3,101 +3,62 @@ package cz.muni.fi.pv256.movio2.uco_396496.myapplication;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Parcel;
-import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
-import android.util.DisplayMetrics;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity
         implements MainFragment.OnMovieSelectListener {
 
     private boolean mTwoPane;
-    private Button btn;
-    private Button btn2;
-    private Button btn3;
     private Context mContext;
     private Movie movie;
+    ArrayList<Movie> movies;
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mContext = getApplicationContext();
-        btn = (Button) findViewById(R.id.button);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                movie = new Movie(Parcel.obtain());
-                movie.setBackdrop("backdrop");
-                movie.setCoverPath("path");
-                movie.setPopularity(10.0f);
-                movie.setTitle("Movie 1");
-                if (mTwoPane) {
-                    FragmentManager fm = getSupportFragmentManager();
+        RecyclerView rvMovies = (RecyclerView) findViewById(R.id.rvMovies);
+        TextView emptyView = (TextView) findViewById(R.id.empty_view);
 
-                    DetailFragment fragment = DetailFragment.newInstance(movie);
-                    fm.beginTransaction()
-                            .replace(R.id.movie_detail_container, fragment, DetailFragment.TAG)
-                            .commit();
+        movies = new ArrayList<>();
+        movie = new Movie(Parcel.obtain());
+        movie.setTitle("Blade Runner 2049");
+        movies.add(movie);
 
-                } else {
-                    Intent intent = new Intent(mContext, MovieDetailActivity.class);
-                    intent.putExtra(MovieDetailActivity.EXTRA_MOVIE, movie);
-                    startActivity(intent);
-                }
-            }
-        });
-        btn = (Button) findViewById(R.id.button2);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                movie = new Movie(Parcel.obtain());
-                movie.setBackdrop("backdrop");
-                movie.setCoverPath("path");
-                movie.setPopularity(10.0f);
-                movie.setTitle("Movie 2");
-                if (mTwoPane) {
-                    FragmentManager fm = getSupportFragmentManager();
+        movie = new Movie(Parcel.obtain());
+        movie.setTitle("Thor: Ragnarok");
+        movies.add(movie);
 
-                    DetailFragment fragment = DetailFragment.newInstance(movie);
-                    fm.beginTransaction()
-                            .replace(R.id.movie_detail_container, fragment, DetailFragment.TAG)
-                            .commit();
+        movie = new Movie(Parcel.obtain());
+        movie.setTitle("It");
+        movies.add(movie);
 
-                } else {
-                    Intent intent = new Intent(mContext, MovieDetailActivity.class);
-                    intent.putExtra(MovieDetailActivity.EXTRA_MOVIE, movie);
-                    startActivity(intent);
-                }
-            }
-        });
-        btn = (Button) findViewById(R.id.button3);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                movie = new Movie(Parcel.obtain());
-                movie.setBackdrop("backdrop");
-                movie.setCoverPath("path");
-                movie.setPopularity(10.0f);
-                movie.setTitle("Movie 3");
-                if (mTwoPane) {
-                    FragmentManager fm = getSupportFragmentManager();
+        if (movies.isEmpty()) {
+            rvMovies.setVisibility(View.GONE);
+            emptyView.setVisibility(View.VISIBLE);
+        }
+        else {
+            rvMovies.setVisibility(View.VISIBLE);
+            emptyView.setVisibility(View.GONE);
+        }
 
-                    DetailFragment fragment = DetailFragment.newInstance(movie);
-                    fm.beginTransaction()
-                            .replace(R.id.movie_detail_container, fragment, DetailFragment.TAG)
-                            .commit();
-
-                } else {
-                    Intent intent = new Intent(mContext, MovieDetailActivity.class);
-                    intent.putExtra(MovieDetailActivity.EXTRA_MOVIE, movie);
-                    startActivity(intent);
-                }
-            }
-        });
+        MoviesAdapter adapter = new MoviesAdapter(this, movies, mTwoPane);
+        rvMovies.setAdapter(adapter);
+        rvMovies.setLayoutManager(new LinearLayoutManager(this));
+        RecyclerView.ItemDecoration itemDecoration = new
+                DividerItemDecoration(this, DividerItemDecoration.VERTICAL);
+        rvMovies.addItemDecoration(itemDecoration);
 
         if (findViewById(R.id.movie_detail_container) != null) {
             mTwoPane = true;
